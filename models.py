752cf313aa23471c160ed5bd8bf1a8fbe6939b11
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-06-15 10:48:23 -0400

    Relations one to one, one to many and many to many

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-Column, Integer, Float, DateTime, String, Text = None
+Column, Integer, Float, DateTime, String, Text, ForeignKey, relationship = None
 db = None
 
 class Base:
@@ -14,12 +14,28 @@ class User(Base):
     username = Column(String(120), nullable=False, unique=True)
     password = Column(String(120), nullable=False)
     created_at = Column(DateTime(), default=datetime.now())
+    roles_id = Column(Integer, ForeignKey('roles.id'), nullable=False)
+    role = relationship('Role', back_populate="users", uselist=False) # [<Role 1>] => <Role 1>
     
     def serialize(self):
         return {
             "id": self.id,
             "username": self.username,
             "created_at": self.created_at
+        }
+        
+
+class Role(Base):
+    __tablename__ = "roles"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    #users = relationship('Users', backref="role")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
         }
           
 """ INSERT """
@@ -70,3 +86,11 @@ if error:
 else:
     db.session.commit() # Confirma los cambios
 
+
+
+# Using Relationship
+user = User.query.get(1)
+user.role.name
+
+role = Role.query.get(1)
+role.users
